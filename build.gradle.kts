264d7a0a67bf7401b179c3a2db34730ee9767562
Mykhailo Dorokhin <adjorno@gmail.com>
2026-01-12 21:31:32 +0100

    Add buildkonfig plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -10,11 +12,26 @@ plugins {
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.buildkonfig) apply false
 }
 
 subprojects {
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
     apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    plugins.withId("com.codingfeline.buildkonfig") {
+        configure<com.codingfeline.buildkonfig.gradle.BuildKonfigExtension> {
+            packageName = "com.ifochka.jufk"
+
+            val versionName = System.getenv("VERSION_NAME")
+                ?: project.findProperty("VERSION_NAME")?.toString()
+                ?: "local build"
+
+            defaultConfigs {
+                buildConfigField(STRING, "VERSION_NAME", versionName)
+            }
+        }
+    }
 
     configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
         debug.set(true)
