34748692f1196093621c147bb1b29f106aa12ebf
Mykhailo Dorokhin <adjorno@gmail.com>
2026-02-12 10:35:30 +0100

    Create Dimensions

--- composeApp/src/commonMain/kotlin/com/ifochka/jufk/App.kt
+++ composeApp/src/commonMain/kotlin/com/ifochka/jufk/App.kt
@@ -1,6 +1,5 @@
 package com.ifochka.jufk
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,9 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,12 +20,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.ifochka.jufk.data.Content
 import com.ifochka.jufk.ui.JufkTheme
 import com.ifochka.jufk.ui.components.Footer
 import com.ifochka.jufk.ui.components.HeroSection
+import com.ifochka.jufk.ui.theme.Dimensions
 import com.ifochka.jufk.youtube.YoutubeSection
 import com.ifochka.jufk.youtube.YoutubeVideo
 import com.ifochka.jufk.youtube.YoutubeVideoDataSourceFromApi
@@ -45,29 +47,38 @@ fun App() {
         videos = youtubeVideosDataSource.fetchVideos(Content.YOUTUBE_PLAYLIST_ID)
     }
     JufkTheme {
-        Scaffold(
-            bottomBar = {
-                Footer(
-                    links = Content.SocialLinks,
-                    modifier = Modifier.navigationBarsPadding(),
-                )
-            },
-        ) { paddingValues ->
-            Box(
-                modifier = Modifier.fillMaxSize().background(Color.Black).padding(paddingValues),
-                contentAlignment = Alignment.TopCenter,
-            ) {
-                Column(
-                    modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    HeroSection()
-                    Spacer(modifier = Modifier.height(32.dp))
-
-                    YoutubeSection(
-                        isLoading = isVideosLoading,
-                        videos = videos,
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            Scaffold(
+                bottomBar = {
+                    Footer(
+                        links = Content.SocialLinks,
+                        modifier = Modifier.navigationBarsPadding(),
                     )
+                },
+            ) { paddingValues ->
+                Box(
+                    modifier = Modifier.fillMaxSize().padding(paddingValues),
+                    contentAlignment = Alignment.TopCenter,
+                ) {
+                    Box(
+                        modifier = Modifier.widthIn(max = Dimensions.MAX_CONTENT_WIDTH).fillMaxSize(),
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+                            HeroSection()
+                            Spacer(modifier = Modifier.height(32.dp))
+
+                            YoutubeSection(
+                                isLoading = isVideosLoading,
+                                videos = videos,
+                            )
+                        }
+                    }
                 }
             }
         }
--- composeApp/src/commonMain/kotlin/com/ifochka/jufk/ui/JufkTheme.kt
+++ composeApp/src/commonMain/kotlin/com/ifochka/jufk/ui/JufkTheme.kt
@@ -1,44 +1,29 @@
 package com.ifochka.jufk.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun JufkTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        content = {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background,
-            ) {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.TopCenter,
-                ) {
-                    Box(
-                        modifier = Modifier.widthIn(max = 1250.dp).fillMaxSize(),
-                    ) {
-                        content()
-                    }
-                }
-            }
-        },
-        colorScheme = JufkColorScheme,
+        colorScheme = DarkColorScheme,
+        content = content,
     )
 }
 
-val KotlinPurple = Color(0xFF8A45FC)
-val JufkColorScheme = darkColorScheme(
-    background = Color.Black,
-    onBackground = Color.White,
-    primary = KotlinPurple,
-)
+private val Purple = Color(0xFF7F52FF)
+private val Orange = Color(0xFFF88909)
+
+private val DarkColorScheme =
+    darkColorScheme(
+        primary = Purple,
+        secondary = Orange,
+        background = Color(0xFF121212), // A deeper black for the background
+        surface = Color(0xFF1E1E1E), // A slightly lighter black for surfaces
+        onPrimary = Color.White,
+        onSecondary = Color.White,
+        onBackground = Color.White,
+        onSurface = Color.White,
+    )
